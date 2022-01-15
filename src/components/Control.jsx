18c58cbc09b1444c12af281bcc0174b3ffc5bb32
Ben Wilson <benjaminw1030@gmail.com>
2022-01-14 17:24:45 -0800

    created responsive drawer sidebar

---
+++ src/components/Control.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "./Sidebar";
+import Main from "./Main";
+
+function Control() {
+  return (
+    <>
+      <Sidebar />
+      <Main />
+    </>
+  );
+}
+
+export default Control;
